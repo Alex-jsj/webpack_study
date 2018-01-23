a862235a119fddea3dc613a93b32bd0e348080ef
alex <chenzeyongjsj@163.com>
2018-01-23 23:07:08 +0800

    HTML文件的发布

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const uglify = require('uglifyjs-webpack-plugin'); //js代码压缩插件
+const htmlPlugin = require('html-webpack-plugin');
 module.exports = {
     //入口文件的配置项
     entry: {
@@ -11,9 +13,33 @@ module.exports = {
         filename: '[name].js'
     },
     //模块：例如解读CSS,图片如何转换，压缩
-    module: {},
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    },
     //插件，用于生产模版和各项功能
-    plugins: [],
+    plugins: [
+        new uglify(),
+        new htmlPlugin({
+            minify: { //对html文件进行压缩，removeAttrubuteQuotes是去掉属性的双引号。
+                removeAttributeQuotes: true
+            },
+            hash: true, //为了开发中js有缓存效果，加入hash，可以有效避免缓存JS
+            template: './src/index.html' //要打包的html模版路径和文件名称
+
+        })
+    ],
     //配置webpack开发服务功能
-    devServer: {}
+    devServer: {
+        //设置基本目录结构
+        contentBase: path.resolve(__dirname, 'dist'),
+        //服务器的IP地址，可以使用IP也可以使用localhost
+        host: 'localhost',
+        //服务端压缩是否开启
+        compress: true,
+        //配置服务端口号
+        port: 1717
+    }
 }
